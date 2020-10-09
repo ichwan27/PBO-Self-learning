fabad27979183a43e07eba6aee84e90ae1c89741
ichwan27 <54470305+ichwan27@users.noreply.github.com>
2020-10-09 20:51:49 +0700

    Create mantap.java

---
+++ mantap.java
@@ -0,0 +1,8 @@
+public class test{
+	public static void main(String[] args){
+		 float floatValue = 3.14f;
+         double doubleValue = 4242.12331;
+		System.out.println("floatValue= " + floatValue +
+                        " doubleValue= " + doubleValue);
+	}
+}
